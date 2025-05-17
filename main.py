4fc3460608cb2cc63d634ea6ee0fcad4783e011f
Asher <mahdis.ghadamyari@gmail.com>
2025-05-17 22:46:43 +0330

    [rmv] organization check

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+BOT_TOKEN = os.getenv("bot_token")
+CHAT_ID = os.getenv("chat_id")  # میتونی این رو ثابت بذاری یا از پیام‌ها بخونی
+PROJECT_MAPPER = {
+    1:"بازیگوشی"
+}
+def send_telegram_message(chat_id, message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    response = requests.post(url, data=data)
+    return response
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = request.get_json()
+
+    if 'message' in update:
+        text = update['message']
+        project = update.get('project_id', 1)
+
+        send_telegram_message(CHAT_ID, f'''
+نوتیفیکیشن پروژه : {PROJECT_MAPPER.get(project)}
+    --------------------------------------------
+    {text}
+    ''')
+
+    return jsonify({"ok": True})
+
+if __name__ == '__main__':
+    app.run(port=5000)
